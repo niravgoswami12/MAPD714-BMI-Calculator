911ec7a286a55a0f78e5d8eac26ae3325e04fa09
Nirav Goswami <niravgoswami12@gmail.com>
2022-12-06 18:57:58 -0500

    first commit

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by Nirav Goswami on 2022-12-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
